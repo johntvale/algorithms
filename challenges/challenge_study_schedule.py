01b250066da64326cae6aad7ee762cbbc47785ee
johntvale <johntvale@gmail.com>
2022-07-02 16:36:12 -0400

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_in_target_time = 0
+    try:
+        for student_time_track in permanence_period:
+            if student_time_track[0] <= target_time <= student_time_track[1]:
+                students_in_target_time += 1
+        return students_in_target_time
+    except TypeError:
+        return None
