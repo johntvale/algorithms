f687c202307ab9285e9e38e19aedf37eec392ed4
johntvale <johntvale@gmail.com>
2022-07-02 19:26:18 -0400

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word: return False
+    if word[low_index] != word[high_index]: return False
+    if len(word) == 1: return True
+    if (low_index <= high_index):
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
