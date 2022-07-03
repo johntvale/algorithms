7c9266f715046b787d2b8e68ee7197091817f67a
johntvale <johntvale@gmail.com>
2022-07-03 15:05:25 -0400

    Requisito 3 atualizado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,15 +2,18 @@ def is_anagram(first_string, second_string):
     n = len(first_string)
     if ((not first_string) or (not second_string)): return False
     if (len(first_string) != len(second_string)): return False
+    result = True
 
     first_word = first_string.lower()
-    second_word = second_string.lower()
+    second_word_list = list(second_string.lower())
     new_word = ""
 
     for index in range(0,n):
-        for index_second_word in range(0,n):
-            if second_word[index_second_word] == first_word[index]:
-                new_word += second_word[index_second_word]
-                second_word.replace(second_word[index_second_word], "")
+        if first_word[index] in second_word_list:
+            new_word += first_word[index]
+            second_word_list.remove(first_word[index])
     
-    return first_word == new_word
+    if (first_word != new_word):
+        result = False
+    
+    return result
