bddde92f0421374b8da092629b19ce8236aa0489
johntvale <johntvale@gmail.com>
2022-07-05 16:28:32 -0400

    correção do flake8 + redução de complexidade no requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,19 +1,17 @@
 def is_anagram(first_string, second_string):
     n = len(first_string)
-    if ((not first_string) or (not second_string)): return False
-    if (len(first_string) != len(second_string)): return False
-    result = True
+    if ((not first_string) or (not second_string)):
+        return False
+    if (len(first_string) != len(second_string)):
+        return False
 
-    first_word = first_string.lower()
-    second_word_list = list(second_string.lower())
+    first_word = first_string.upper()
+    second_word_list = list(second_string.upper())
     new_word = ""
 
-    for index in range(0,n):
+    for index in range(0, n):
         if first_word[index] in second_word_list:
             new_word += first_word[index]
             second_word_list.remove(first_word[index])
-    
-    if (first_word != new_word):
-        result = False
-    
-    return result
+
+    return first_word == new_word
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if not word: return False
-    if word[low_index] != word[high_index]: return False
-    if len(word) == 1: return True
+    if not word:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if len(word) == 1:
+        return True
     if (low_index <= high_index):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return True
